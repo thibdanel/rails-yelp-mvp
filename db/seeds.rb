374dabaf26f44ee9c514ef2f5a753fa15aee969e
Thibault Danel <thibdanel@hotmail.fr>
2022-02-16 15:32:37 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+puts 'Creating 10 fake restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: 'italian'
+  )
+  restaurant.save!
+end
+puts 'Finished!'
